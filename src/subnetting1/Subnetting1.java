081edcf77051c78d93a73a6d970bb8d6e0883811
Ingo Mörl <sag@ich.net>
2021-08-17 17:52:40 +0200

    Start

---
+++ src/subnetting1/Subnetting1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package subnetting1;
+
+/**
+ *
+ * @author moerle
+ */
+public class Subnetting1 {
+ 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        StartFenster fenster1;
+        fenster1 = new StartFenster();
+        //fenster1.show();
+        fenster1.setVisible(true);
+    }
+}
